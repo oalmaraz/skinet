616d34024059ad4fc3610f42a92ba868694cd707
oalmaraz <oswaldo_alm@hotmail.com>
2023-12-28 21:23:36 -0600

    End of section 11

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
+  {path:'test-error', component: TestErrorComponent},
+  {path:'not-found', component: NotfoundComponent},
+  {path:'server-error', component: ServerErrorComponent},
   {path:'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule) },
   {path:'**', redirectTo: '', pathMatch: 'full'},
 ];
